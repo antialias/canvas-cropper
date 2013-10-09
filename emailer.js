801689c1761bbc4be0a7ec2f93f28edf67a2fed0
antialias <thomas.hallock@kaplan.com>
2013-10-09 15:51:20 -0400

    POD-2826 pleasing the linter

--- emailer.js
+++ emailer.js
@@ -15,7 +15,7 @@ define(['knockout', '/js/lp/lib/utils.js'], function (ko, lpUtils) {
 					to: ko.observable(args.to),
 					from: ko.observable(args.from),
 					copyMe: ko.observable(false),
-					message: ko.observable(args.message),
+					message: ko.observable(args.message)
 				},
 				attachedHTML: ko.observable(args.attachedHTML)
 			};
@@ -61,5 +61,5 @@ define(['knockout', '/js/lp/lib/utils.js'], function (ko, lpUtils) {
 				model: emailModel
 			};
 		}
-	}
+	};
 });
